d10bd5c56996438f967b755e3583352248b6d6d8
Andre Cardoso <andrecardosodev@gmail.com>
2016-06-18 21:28:46 -0300

    Placed Module.php inside it's namespace

--- Module.php
+++ Module.php
@@ -1,51 +1,8 @@
 <?php
 
-namespace SendGridTransportModule;
-
-use Exception;
-use SendGrid;
-
 /**
- * Class Module
- * @package SendGrid
+ * This file is placed here for compatibility with Zendframework 2's ModuleManager.
+ * It allows usage of this module even without composer.
+ * The original Module.php is in 'src/SendGridTransportModule' in order to respect PSR-4
  */
-class Module
-{
-
-    /**
-     * @return array
-     */
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__
-                ),
-            ),
-        );
-    }
-
-    /**
-     * @return array
-     */
-    public function getServiceConfig()
-    {
-        return array(
-            'factories' => array(
-                'SendGridTransport' => function ($sm) {
-                    $config = $sm->get('config');
-
-                    if (!isset($config['sendgrid'])) {
-                        throw new Exception('You must to copy the file "sendgrid.global.php.dist" to your config/autoload and set your API Key');
-                    }
-                    
-                    $sendGrid = new SendGrid($config['sendgrid']['api_key']);
-                    $email = new SendGrid\Email();
-
-                    return new SendGridTransport($sendGrid, $email);
-                },
-            ),
-        );
-    }
-}
+require_once __DIR__ . '/src/SendGridTransportModule/Module.php';
